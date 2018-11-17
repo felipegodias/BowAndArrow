4150bac2c49ee1adbdd15bdb204bdb56aa2783c7
felipegodias <felipe.g.o.dias@outlook.com>
2018-11-17 10:38:21 -0300

    Added blood particles to the bunnies.

--- Assets/Src/Bunny.cs
+++ Assets/Src/Bunny.cs
@@ -6,6 +6,9 @@ public class Bunny : MonoBehaviour
     [SerializeField]
     private float m_speed;
 
+    [SerializeField]
+    private ParticleSystem m_bloodSplash;
+
     private void Update()
     {
         transform.position += transform.up * m_speed * Time.deltaTime;
@@ -19,6 +22,8 @@ public class Bunny : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
         if (collider2D.gameObject.GetComponent<Arrow>() == null) { return; }
+
+        if (m_bloodSplash != null) { Instantiate(m_bloodSplash, transform.position, Quaternion.identity); }
 
         Destroy(gameObject);
     }
---
+++ Assets/Src/ParticleDestroyer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ParticleDestroyer : MonoBehaviour
+{
+
+    private ParticleSystem m_particleSystem;
+
+    private void Awake()
+    {
+        m_particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (!m_particleSystem.IsAlive(true)) { Destroy(gameObject); }
+    }
+
+}
